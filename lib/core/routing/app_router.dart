97df55bdeead0d424e3ea9b62e9b65e508f9a066
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-06-17 14:29:35 +0300

    splash screen done and fix error in android 12

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:doctor_app/core/routing/routes.dart';
 import 'package:doctor_app/features/on_boarding/ui/screens/on_boarding_screen.dart';
+import 'package:doctor_app/features/splash/ui/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
@@ -9,7 +10,10 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (context) => const OnBoardingScreen(),
         );
-
+      case Routes.splashScreen:
+        return MaterialPageRoute(
+          builder: (context) => const SplashScreen(),
+        );
       default:
         return MaterialPageRoute(
           builder: (context) => const Scaffold(
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,3 +1,5 @@
 class Routes {
   static const String onBoardingScreen = "/onBoardingScreen";
+  static const String splashScreen = "/splashScreen";
+
 }
